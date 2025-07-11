33bf432a493375b1c0e96c9eeb91f0f4ddfac9b8
Hana Bobko <hanamoraesbobko@gmail.com>
2025-07-10 21:42:40 -0300

    :sparkles:feat:configurando evaluationController

--- src/routes/evaluationRoutes.js
+++ src/routes/evaluationRoutes.js
@@ -4,6 +4,6 @@ import verifiToken from "../middlewares/auth.js";
 import { createEvaluation, getMyEvaluationsByEvent } from "../controllers/evaluationController.js";
 
 router.post("/evaluations", verifiToken, createEvaluation);
-router.get("/evaluations/my/:eventId", verifiToken, getMyEvaluationsByEvent);
+router.get("/evaluations/:eventId", verifiToken, getMyEvaluationsByEvent);
 
 export default router;
--- src/routes/rankingRoutes.js
+++ src/routes/rankingRoutes.js
@@ -3,4 +3,4 @@ import { Router } from "express";
 const router = Router();
 import { getEventRanking } from "../controllers/rankingController.js";
 
-router.get("/events/:eventId/ranking", verifiToken, getEventRanking);
+router.get("/events/:eventId", verifiToken, getEventRanking);
